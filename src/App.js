8003625440d34717491df4019f70ce2972341949
shinyuy <shinyuy9@gmail.com>
2019-02-02 19:43:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './resources/styles.css';
+import Header from './components/header,footer/Header';
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{ height:"1500px", background:'cornflowerblue'}}>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
